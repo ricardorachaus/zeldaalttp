10cd846a5d0788c0d3a776c5ade80f9bd2449e4e
Ricardo <ricardorachaus@gmail.com>
2018-03-03 19:09:33 -0300

    Start making home scene

---
+++ The Legend Of Zelda A Link To The Past/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player;
+    public float upperBound;
+    public float lowerBound;
+    public float rightBound;
+    public float leftBound;
+
+    private Vector3 offset;
+
+    void Start() {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate() {
+        var newPosition = player.transform.position + offset;
+        var horizontalBounds = newPosition.x < rightBound &&
+                               newPosition.x > leftBound;
+        var verticalBounds = newPosition.y < upperBound &&
+                             newPosition.y > lowerBound;
+
+        if (horizontalBounds && verticalBounds) {
+            transform.position = newPosition;
+        }
+    }
+
+}
